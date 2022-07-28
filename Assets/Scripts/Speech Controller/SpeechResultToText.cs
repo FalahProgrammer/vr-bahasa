d4e6a4290c6e49b42d5bfc36e609ab8e2e152573
dimzki <dimaz.a.zaki@gmail.com>
2022-07-28 09:48:48 +0700

    Penambahan Scenario

--- Assets/Scripts/Speech Controller/SpeechResultToText.cs
+++ Assets/Scripts/Speech Controller/SpeechResultToText.cs
@@ -19,16 +19,7 @@ public class SpeechResultToText : MonoBehaviour
         
         //var confidance = 0f;
         var index = 0;
-        
-        /*for (int i = 0; i < result.Phrases.Length; i++)
-        {
-            if (confidance < result.Phrases[i].Confidence)
-            {
-                confidance = result.Phrases[i].Confidence;
-                index = i;
-            }
-        }*/
-        
+
         ResultText.text = result.Phrases[index].Text;
     }
 }
